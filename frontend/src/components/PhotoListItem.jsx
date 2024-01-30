8542fe38f0600d5f2cf7dbf9c291066e04dcb16a
Katelynn <grayspence2@hotmail.com>
2024-01-30 16:41:21 -0500

    added styles to the PhotoListItem component.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,11 +5,13 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
 
   return (
-    <div>
-    <img src={props.sampleData.imageSource}></img>
-    <img src={props.sampleData.profile}></img>
-    <h1>{props.sampleData.username}</h1>
-    <p>{props.sampleData.location.city}, {props.sampleData.location.country}</p>
+    <div className="photo-list__item">
+    <img src={props.sampleData.imageSource} alt="Main" className="photo-list__image"></img>
+    <div className="photo-list__user-info">
+    <img src={props.sampleData.profile} className="photo-list__user-profile"></img>
+    <p>{props.sampleData.username}</p>
+    <p className="photo-list__user-location">{props.sampleData.location.city}, {props.sampleData.location.country}</p>
+    </div>
     </div>
   )
 };
