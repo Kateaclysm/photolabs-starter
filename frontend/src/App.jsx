8b84813ad90ef20f6f89ff79304efef2ddfebb8c
Katelynn <grayspence2@hotmail.com>
2024-02-01 15:40:22 -0500

    Tentative global state for favourited photos added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import topics from './mocks/topics'
 import HomeRoute from './components/HomeRoute';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [favoritePhotos, setFavoritePhotos] = useState([]);
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics}/>
