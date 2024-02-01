2167303f0534e1aaf93b67c5746e2c4d5cfa4d04
Katelynn <grayspence2@hotmail.com>
2024-01-31 19:50:17 -0500

    Added the navbar component as well as made it render in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import PhotoList from './components/PhotoList'
 import './App.scss';
 import TopicListItem from './components/TopicListItem'
 import TopicList from './components/TopicList'
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList/>
+      <TopNavigation/>
       <PhotoList/>
     </div>
   );
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -5,7 +5,7 @@ import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
   return (
-    <div className="topic-list__item &:hover">
+    <div className="topic-list__item">
     <span>{props.title}</span>
     </div>
   );
