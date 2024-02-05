7a7777c6267e888848f314e396c35cc9c4de81ca
Katelynn <grayspence2@hotmail.com>
2024-02-05 15:19:46 -0500

    added functional close button to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <div className="photo-details-modal">
-      <section className="photo-details-modal__header"/>
-      <section className="photo-details-modal_top-bar"/>
-      <button className="photo-details-modal__close-button"/>
-      <img src={closeSymbol} alt="close symbol" />
-      
+      isModalOpen ? (
+      <div className="photo-details-modal">
+      <section className="photo-details-modal__header">
+        <section className="photo-details-modal_top-bar"/>
+        <button className="photo-details-modal__close-button" onClick={closeModal} type="button">
+        <img src={closeSymbol} alt="close symbol"/>
+        </button>
+        </section>
       <img className="photo-details-modal__image"/>
       <p className="photo-details-modal__photographer-details"/>
-      
-    </div>
+      </div>
+    ) : null
   )
 };
 
